72e5742e8e26a22502e316c9f1a46ee02aedbce7
DevChirayu <chirayupatil23@gmail.com>
2023-12-23 22:43:03 +0530

    fs feature

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var http = require('http');
+const fs = require('fs');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write("Hello World!");
-    res.end();
+
+    // File System Integrated
+    fs.readFile('./welcome.html',(err, data)=>{
+        if(err) throw err;
+        res.write(data);
+        res.write("Hello World!");
+        res.end();
+    });
+    
 
 }).listen(8416, function () {
     console.log('Server running at http://127.0.0.1:8416/');
